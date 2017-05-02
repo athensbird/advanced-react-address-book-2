e7e348c7c486b1ac2ca3a86d26955bb563f7185f
Mark Wang <athensbird@Marsbook.local>
2017-05-02 17:04:01 -0500

    Implemented available/selected users functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import React from "react";
+import React, {Component} from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import ListOfUsers from "./ListOfUsers";
 import PropTypes from "prop-types";
 
-function App(props) {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedList: []
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h2>Available Users</h2>
+        <div>
+          <ListOfUsers users={this.props.users} moveUsers={(para) => {
+            this.setState({
+              selectedList: [
+                ...this.state.selectedList,
+                para
+              ]
+            });
+          }} />
+        </div>
+        <br />
+        <h2>Selected Users</h2>
+        <div>
+          <ListOfUsers users={this.state.selectedList} />
+        </div>
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-      <ListOfUsers users={props.users} />
-    </div>
-  );
+    );
+  }
 }
 
 App.propTypes = {
