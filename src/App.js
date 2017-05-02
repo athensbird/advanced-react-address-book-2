a2a2c48ac0bd982ca825b929d359e5197bae6340
Mark Wang <athensbird@Marsbook.local>
2017-05-02 15:38:37 -0500

    Separate ListOfUsers & UserDetail

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import ListOfUsers from "./ListOfUsers";
+import PropTypes from "prop-types";
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <div className="App-header">
@@ -12,8 +14,15 @@ function App() {
       <p className="App-intro">
         To get started, edit <code>src/App.js</code> and save to reload.
       </p>
+      <ListOfUsers users={props.users} />
     </div>
   );
 }
+
+App.propTypes = {
+  users: PropTypes.array.isRequired
+};
+
+
 
 export default App;
